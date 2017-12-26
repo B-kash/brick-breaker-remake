fabfc8a10d4f817a5754067d2303381294fe00f3
b-kash <bikashchapagain047@gmail.com>
2017-12-26 23:23:44 +0545

    ball now bounces on each wall collision

---
+++ core/src/com/brick/brickbreaker/constants/Constants.java
@@ -0,0 +1,14 @@
+package com.brick.brickbreaker;
+
+/**
+ * Created by bikash on 12/26/17.
+ */
+
+public final class Constants {
+    public static int WALL_TOP = 1;
+    public static int WALL_BOTTOM = 2;
+    public static int WALL_LEFT = 3;
+    public static int WALL_RIGHT = 4;
+    public static int WALL_NONE = 0;
+    public static float DEFAULT_BALL_SPEED = 1;
+}
---
+++ core/src/com/brick/brickbreaker/entity/Ball.java
@@ -0,0 +1,65 @@
+package com.brick.brickbreaker.Entity;
+import com.badlogic.gdx.math.Circle;
+import com.badlogic.gdx.math.Vector2;
+
+/**
+ * Created by bikash on 12/26/17.
+ */
+
+public class Ball extends Circle {
+    float xSpeed,ySpeed;
+//    float direction;
+
+    public Ball() {
+    }
+
+    public Ball(float xSpeed, float ySpeed) {
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public Ball(float x, float y, float radius, float xSpeed, float ySpeed) {
+        super(x, y, radius);
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public Ball(Vector2 position, float radius, float xSpeed, float ySpeed) {
+        super(position, radius);
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public Ball(Circle circle, float xSpeed, float ySpeed) {
+        super(circle);
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public Ball(Vector2 center, Vector2 edge, float xSpeed, float ySpeed) {
+        super(center, edge);
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public float getxSpeed() {
+        return xSpeed;
+    }
+
+    public void setxSpeed(float xSpeed) {
+        this.xSpeed = xSpeed;
+    }
+
+    public float getySpeed() {
+        return ySpeed;
+    }
+
+    public void setySpeed(float ySpeed) {
+        this.ySpeed = ySpeed;
+    }
+
+    public void move(){
+        x+=xSpeed;
+        y+=ySpeed;
+    }
+}
